8c864bcad78907862c879513a4e09740318d88c3
dana-savinova <savinovadana@ya.ru>
2020-01-01 11:16:59 +0300

    Стилизует мобильное меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,22 @@
 var headerNav = document.querySelector('.page-header__nav');
 var navToggle = document.querySelector('.page-header__toggle');
+var pageHeader = document.querySelector('.page-header');
 
 if (headerNav.classList.contains('page-header__nav--nojs')) {
   headerNav.classList.remove('page-header__nav--nojs');
   headerNav.classList.add('page-header__nav--closed');
+  pageHeader.classList.add('page-header--dark');
+  navToggle.classList.add('page-header__toggle--closed');
 };
 
 navToggle.addEventListener('click', function() {
   if (headerNav.classList.contains('page-header__nav--closed')) {
     headerNav.classList.remove('page-header__nav--closed');
     navToggle.classList.remove('page-header__toggle--closed');
+    pageHeader.classList.remove('page-header--dark');
   } else {
     headerNav.classList.add('page-header__nav--closed');
     navToggle.classList.add('page-header__toggle--closed');
+    pageHeader.classList.add('page-header--dark');
   }
 });
