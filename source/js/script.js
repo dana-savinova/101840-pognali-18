d9e86249b5e2de64bbd320c5d87aa57bf3c0224f
dana-savinova <savinovadana@ya.ru>
2020-01-17 10:52:42 +0300

    Добавляет показ попапа с тарифами и ретина фоны для блока интро

--- source/js/script.js
+++ source/js/script.js
@@ -20,3 +20,24 @@ navToggle.addEventListener('click', function() {
     pageHeader.classList.add('page-header--dark');
   }
 });
+
+
+var ratesLink = document.querySelector(".rates__link");
+var ratesPopup = document.querySelector(".biz-rates");
+var ratesCloseBtn = document.querySelector(".biz-rates__btn");
+
+ratesLink.addEventListener("click", function(evt){
+  evt.preventDefault();
+  ratesPopup.classList.add("biz-rates--show");
+});
+
+ratesCloseBtn.addEventListener("click", function(evt){
+  evt.preventDefault();
+  ratesPopup.classList.remove("biz-rates--show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    ratesPopup.classList.remove("biz-rates--show");
+  }
+});
