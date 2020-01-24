1888278a97d61ce238e67792251807ede21d6458
dana-savinova <savinovadana@ya.ru>
2020-01-24 11:49:09 +0300

    Изменяет префикс для минифицированных файлов в сборке

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task("css", function () {
     ]))
     .pipe(gulp.dest("build/css"))
     .pipe(csso())
-    .pipe(rename("style-min.css"))
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
