33e31afd87a41bdda1efe28494d326b63396c716
dana-savinova <savinovadana@ya.ru>
2020-01-25 14:43:08 +0300

    Заменяет кавычки

--- source/js/script.js
+++ source/js/script.js
@@ -1,23 +1,23 @@
-var headerNav = document.querySelector('.page-header__nav');
-var navToggle = document.querySelector('.page-header__toggle');
-var pageHeader = document.querySelector('.page-header');
+var headerNav = document.querySelector(".page-header__nav");
+var navToggle = document.querySelector(".page-header__toggle");
+var pageHeader = document.querySelector(".page-header");
 
-if (headerNav.classList.contains('page-header__nav--nojs')) {
-  headerNav.classList.remove('page-header__nav--nojs');
-  navToggle.classList.remove('page-header__toggle--nojs');
-  headerNav.classList.add('page-header__nav--closed');
-  pageHeader.classList.add('page-header--dark');
-  navToggle.classList.add('page-header__toggle--closed');
+if (headerNav.classList.contains("page-header__nav--nojs")) {
+  headerNav.classList.remove("page-header__nav--nojs");
+  navToggle.classList.remove("page-header__toggle--nojs");
+  headerNav.classList.add("page-header__nav--closed");
+  pageHeader.classList.add("page-header--dark");
+  navToggle.classList.add("page-header__toggle--closed");
 };
 
-navToggle.addEventListener('click', function() {
-  if (headerNav.classList.contains('page-header__nav--closed')) {
-    headerNav.classList.remove('page-header__nav--closed');
-    navToggle.classList.remove('page-header__toggle--closed');
-    pageHeader.classList.remove('page-header--dark');
+navToggle.addEventListener("click", function() {
+  if (headerNav.classList.contains("page-header__nav--closed")) {
+    headerNav.classList.remove("page-header__nav--closed");
+    navToggle.classList.remove("page-header__toggle--closed");
+    pageHeader.classList.remove("page-header--dark");
   } else {
-    headerNav.classList.add('page-header__nav--closed');
-    navToggle.classList.add('page-header__toggle--closed');
-    pageHeader.classList.add('page-header--dark');
+    headerNav.classList.add("page-header__nav--closed");
+    navToggle.classList.add("page-header__toggle--closed");
+    pageHeader.classList.add("page-header--dark");
   }
 });
