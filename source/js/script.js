69ee9fbe0cbc646c6e6923d4455f1d8289454233
dana-savinova <savinovadana@ya.ru>
2020-01-25 15:14:23 +0300

    Исправляет работоспособность меню без js

--- source/js/script.js
+++ source/js/script.js
@@ -2,8 +2,9 @@ var headerNav = document.querySelector(".page-header__nav");
 var navToggle = document.querySelector(".page-header__toggle");
 var pageHeader = document.querySelector(".page-header");
 
-if (headerNav.classList.contains("page-header__nav--nojs")) {
+if (pageHeader.classList.contains("page-header--nojs")) {
   headerNav.classList.remove("page-header__nav--nojs");
+  pageHeader.classList.remove("page-header--nojs");
   navToggle.classList.remove("page-header__toggle--nojs");
   headerNav.classList.add("page-header__nav--closed");
   pageHeader.classList.add("page-header--dark");
