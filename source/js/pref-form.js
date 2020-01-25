84a45758747e19073f0b3284b1330bdc8775ba58
dana-savinova <savinovadana@ya.ru>
2020-01-25 18:00:07 +0300

    Вносит правки по замечаниям

--- source/js/pref-form.js
+++ source/js/pref-form.js
@@ -1,8 +1,8 @@
-var formBtn = document.querySelectorAll('.pref-form__title');
+var formBtn = document.querySelectorAll(".pref-form__title");
 
 if(formBtn.length) {
   for (var i = 0; i < formBtn.length; i++) {
-    formBtn[i].addEventListener('click', function (evt) {
+    formBtn[i].addEventListener("click", function (evt) {
       evt.preventDefault();
     });
   }
